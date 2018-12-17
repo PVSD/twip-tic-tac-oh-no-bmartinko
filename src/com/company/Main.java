636bcee966ceefcfd8b5aed34af993dd7de1a84b
bmartinko <bennett.martinko@gmail.com>
2018-12-17 11:16:02 -0500

    I started?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean end = false;
+        char board[] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+        String playerLetter;
+        String computerLetter;
+
+        System.out.println("Welcome to Tic-Tac-Toe with the computer!");
+        for(int i = 0; i < 3; i++){
+            System.out.println("____ | ____ | ____ ");
+        }
+        Scanner playerA = new Scanner(System.in);
+
+
+        while (end = false){
+            int d = (int)(Math.random()) * 100;
+
+            if(d >50){
+            computerLetter = "X";
+            playerLetter = "O";
+            }else{
+            computerLetter = "X";
+            playerLetter = "O";
+            }
+
+
+        }
     }
 }
