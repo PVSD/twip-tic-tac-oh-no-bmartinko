10b16a3ebd84fb84509848d86f437c6cc0cedb90
bmartinko <bennett.martinko@gmail.com>
2018-12-21 11:21:28 -0500

    I added the String Buffer as well as the user inputs and the End messages. Need to work on placing the x and o's, as well as victory conditions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,29 +4,43 @@ public class Main {
 
     public static void main(String[] args) {
         boolean end = false;
-        char board[] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
-        String playerLetter;
-        String computerLetter;
+        String board[][] = new String[3][3];
+        boolean victory = false;
 
+        while(victory == false){
         System.out.println("Welcome to Tic-Tac-Toe with the computer!");
-        for(int i = 0; i < 3; i++){
-            System.out.println("____ | ____ | ____ ");
-        }
-        Scanner playerA = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < 2; i++){
+            for(int z = 0; z < 2; z++){
+                    sb.append("____|");
 
-
-        while (end = false){
-            int d = (int)(Math.random()) * 100;
-
-            if(d >50){
-            computerLetter = "X";
-            playerLetter = "O";
-            }else{
-            computerLetter = "X";
-            playerLetter = "O";
             }
-
-
+            sb.append("____\n");
         }
+        for(int x = 0; x < 2; x++){
+            sb.append("    |");
+        }
+
+        String s = sb.toString();
+        System.out.println(s);
+
+        Scanner playerA = new Scanner(System.in);
+        System.out.println("Enter the row you would like to place your x in");
+        int choiceA = playerA.nextInt();
+        if (choiceA > 2 || choiceA < 0) {
+            System.out.println("Please input a number in the correct range (0-2)");
+        }
+        System.out.println("Enter the column you would like to place the x in");
+        int choiceB = playerA.nextInt();
+        if (choiceB > 2 || choiceB < 0) {
+            System.out.println("Please input a number in the correct range (0-2)");
+        }
+
+     }
+    if (victory == true){
+        System.out.println("A draw has been found");
+        System.out.println("The Computer has won the game!");
+        System.out.println("You have won the game!");
+    }
     }
 }
